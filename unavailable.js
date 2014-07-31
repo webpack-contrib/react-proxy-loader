bc99e32fabb1e233ee36fda09e52c74c3821c40e
Tobias Koppers <tobias.koppers@googlemail.com>
2014-07-31 14:57:53 +0200

    added unavailable alternative
    This can be used for never render the component.

---
+++ unavailable.js
@@ -0,0 +1,20 @@
+var loaderUtils = require("loader-utils");
+
+module.exports = function() {};
+module.exports.pitch = function(remainingRequest) {
+	this.cacheable && this.cacheable();
+	return [
+		'var React = require("react");',
+		'var ReactTextComponent = require("react/lib/ReactTextComponent.js");',
+		'var desc = {',
+		'	loadComponent: function(callback) {},',
+		'	_renderUnavailable: function() {',
+		'		return new ReactTextComponent("")',
+		'	}',
+		'};',
+		'var mixinReactProxy = require(' + JSON.stringify(require.resolve("./mixinReactProxy")) + ');',
+		'mixinReactProxy(desc);',
+		'module.exports = React.createClass(desc);',
+		'module.exports.Mixin = desc;'
+	].join("\n");
+};
