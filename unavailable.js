768458d2a4f5573a13cbe0b2eb0a29ab8c55f43b
Tobias Koppers <tobias.koppers@googlemail.com>
2014-11-26 16:45:48 +0100

    updated unavailable component

--- unavailable.js
+++ unavailable.js
@@ -1,19 +1,13 @@
-var loaderUtils = require("loader-utils");
-
 module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
 	this.cacheable && this.cacheable();
 	return [
 		'var React = require("react");',
-		'var ReactTextComponent = require("react/lib/ReactTextComponent.js");',
 		'var desc = {',
-		'	loadComponent: function(callback) {},',
-		'	_renderUnavailable: function() {',
-		'		return new ReactTextComponent("");',
-		'	}',
+		'	loadComponent: function(callback) {}',
 		'};',
 		'var mixinReactProxy = require(' + JSON.stringify(require.resolve("./mixinReactProxy")) + ');',
-		'mixinReactProxy(desc);',
+		'mixinReactProxy(React, desc);',
 		'module.exports = React.createClass(desc);',
 		'module.exports.Mixin = desc;'
 	].join("\n");
